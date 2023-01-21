d927a6fc167fe2a36d175077d0cd4340ace25329
Cagdas Muldur <cagdasmuldur@gmail.com>
2023-01-21 18:58:36 -0500

    added project. and projectcard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css"
 import {
   BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
   Routes, //replaces "Switch" used till v5
   Route,
 } from "react-router-dom";
@@ -9,19 +11,19 @@ import Header from "./Components/Header";
 import Work from "./Pages/Work";
 import Articles from "./Pages/Articles";
 import About from "./Pages/About";
+import ViralNationProject from "./Pages/Projects/ViralNationProject";
 export default function App () {
  return (
    <div className="App">
      <BrowserRouter>
        <Header />
-       <div className="container">
          <Routes>
            <Route path="/" element={<Home />} />
            <Route path="/work" element={<Work />} />
            <Route path="/articles" element={<Articles />} />
            <Route path="/about" element={<About />} />
+           <Route path="/projects/viralnationproject" element={<ViralNationProject />} />
          </Routes>
-       </div>
      </BrowserRouter>
    </div>
  );
