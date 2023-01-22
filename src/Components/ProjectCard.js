ca87b564fc0e1fea9010f9292aa4d0c92b9fabb2
Cagdas Muldur <cagdasmuldur@gmail.com>
2023-01-21 19:11:08 -0500

    Adding alt tag on images

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -4,19 +4,23 @@ import "../Styles/work.css";
 export default function ProjectCard({projectDetails}) {
   console.log(projectDetails.logo)
   return (
-     <a href={projectDetails.link}>
-    <div className="project">
-      <div className="title_with_logo">
-        <img className="logo" src={projectDetails.logo} alt="viral nation logo"></img>
-        <h4 className="title">{projectDetails.title}</h4>
+    <a href={projectDetails.link}>
+      <div className="project">
+        <div className="title_with_logo">
+          <img
+            className="logo"
+            src={projectDetails.logo}
+            alt="viral nation logo"
+          ></img>
+          <h4 className="title">{projectDetails.title}</h4>
+        </div>
+        <div className="description">{projectDetails.desc}</div>
+        <div className="project_images">
+          <img className="project_image" alt="project-image"></img>
+          <img className="project_image" alt="project-image"></img>
+          <img className="project_image" alt="project-image"></img>
+        </div>
       </div>
-      <div className="description">{projectDetails.desc}</div>
-      <div className="project_images">
-        <img className="project_image"></img>
-        <img className="project_image"></img>
-        <img className="project_image"></img>
-      </div>
-    </div>
-     </a>
+    </a>
   );
 }
