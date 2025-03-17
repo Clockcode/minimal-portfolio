d43032aed691750d19fe34b437370adf6349ee13
clockcode <cagdasmuldur@gmail.com>
2025-03-17 14:54:18 -0400

    fix: link redirect on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,6 @@ export default function Header() {
   }, []);
 
   const handleClick = (event) => {
-    event.preventDefault();
     ReactGA.event({
       category: 'header links',
       action: 'Click',
